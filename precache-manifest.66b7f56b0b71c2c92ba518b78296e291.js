dc1356294c2f7aac924dc325b916252ed9298bde
Tata2222 <tataolcha@gmail.com>
2020-07-03 16:15:48 +0300

    Updates

---
+++ precache-manifest.66b7f56b0b71c2c92ba518b78296e291.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc9811cefe4c15dcd54960c3bb624c12",
+    "url": "/Tata2222/nasa_app/index.html"
+  },
+  {
+    "revision": "abd81b104151cb30c5da",
+    "url": "/Tata2222/nasa_app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e170a6c7b42a1a0d64bc",
+    "url": "/Tata2222/nasa_app/static/js/2.9e1066ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tata2222/nasa_app/static/js/2.9e1066ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd81b104151cb30c5da",
+    "url": "/Tata2222/nasa_app/static/js/main.7d858fe4.chunk.js"
+  },
+  {
+    "revision": "f92abf50441052d78e69",
+    "url": "/Tata2222/nasa_app/static/js/runtime-main.1b3b6465.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Tata2222/nasa_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
