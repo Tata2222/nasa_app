53cac38e5512f2d5a9c335cc084721ed53834f85
Tata2222 <tataolcha@gmail.com>
2020-07-06 15:17:45 +0300

    fixed url

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,10 +1,12 @@
 const today = new Date().toISOString().slice(0,10);
 const fistDayOfMonth = today.slice(0,8).concat('01');
 
-const API_URL = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${fistDayOfMonth}&end_date=${fistDayOfMonth}&api_key=hsDSy2E5Oz9eUPxbvPuAnaAi8w3pIUCJtDiuhg1N`
+const API_URL = `http://api.nasa.gov/neo/rest/v1/feed?start_date=${fistDayOfMonth}&end_date=${fistDayOfMonth}&api_key=hsDSy2E5Oz9eUPxbvPuAnaAi8w3pIUCJtDiuhg1N`
 
 export function fetchAsteroids(url= API_URL) {
-  return fetch(url)
+  const correctUrl = url.replace('http', 'https');
+  console.log(correctUrl)
+  return fetch(correctUrl)
     .then(response => {
       return response.json();
     })
